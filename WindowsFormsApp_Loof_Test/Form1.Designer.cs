420cff94ffdae26f90027b5e231233652e3e73a2
HyunBaeL <seehb126@naver.com>
2025-05-01 13:02:32 +0900

    Add project files.

---
+++ WindowsFormsApp_Loof_Test/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApp_Loof_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button_input = new System.Windows.Forms.Button();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(146, 133);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(206, 194);
+            this.textBox_result.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button_input
+            // 
+            this.button_input.Location = new System.Drawing.Point(284, 68);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(68, 47);
+            this.button_input.TabIndex = 2;
+            this.button_input.Text = "입력";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(146, 79);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(122, 28);
+            this.textBox_input.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.TextBox textBox_input;
+    }
+}
+
---
+++ WindowsFormsApp_Loof_Test/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Loof_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = ""; // input버튼 클릭 할 때 마다 빈칸으로 초기화 😊
+
+            int students;
+            bool check = int.TryParse(textBox_input.Text, out students);
+
+            if (check)
+            {
+                Random random = new Random();
+                string[] student = new string[students];
+
+                for (int i = 0; i < students; i++)
+                {
+                    student[i] = $"학생 {(i + 1).ToString()}의 점수: {random.Next(0, 101)}점 😊";
+                    textBox_result.Text += student[i] + "\r\n";
+                }
+
+            }
+            else
+            {
+                textBox_result.Text = "숫자만 정확히 입력 해주세요 😊";
+            }
+        }
+    }
+}
